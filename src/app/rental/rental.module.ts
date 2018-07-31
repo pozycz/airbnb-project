b90cdc94744f9e14f7ee0cb30d7e24a6e154fa20
Dorian Pozyczka <dorian.pozyczka@gmail.com>
2018-07-31 22:40:23 +0200

    Add proxy conf, adds rental details styles, adds pipes

--- src/app/rental/rental.module.ts
+++ src/app/rental/rental.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { NgPipesModule } from 'ngx-pipes';
 
 
 import { RentalListComponent } from './rental-list/rental-list.component';
@@ -9,6 +11,8 @@ import { RentalComponent } from './rental.component';
 
 import { RentalService } from './shared/rental.service';
 import { RentalDetailComponent } from './rental-detail/rental-detail.component';
+
+import { UppercasePipes } from '../common/pipes/uppercase.pipe';
 
 const routes: Routes = [
   { path: 'rentals',
@@ -25,11 +29,15 @@ const routes: Routes = [
         RentalListComponent,
         RentalListItemComponent,
         RentalComponent,
-        RentalDetailComponent
+        RentalDetailComponent,
+        UppercasePipes
     ],
     imports: [
         CommonModule,
-        RouterModule.forChild(routes)],
+        RouterModule.forChild(routes),
+        HttpClientModule,
+        NgPipesModule
+        ],
     providers: [RentalService]
 })
 export class RentalModule {}
--- src/app/rental/shared/rental.service.ts
+++ src/app/rental/shared/rental.service.ts
@@ -1,80 +1,20 @@
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { Rental } from "./rental.model";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable()
 export class RentalService {
 
-    private rentals: Rental[] = [{
-    id: '1',
-    title: 'Central Apartment',
-    city: 'New York',
-    street: 'Time Squere',
-    category: 'apartment',
-    image: 'http://vim.placeholder.com/350x250',
-    bedrooms: 3,
-    description: 'Very nice apartment',
-    dailyRate: 34,
-    shared: false,
-    createdAt: '24/12/2012'
-  },
-  {
-    id: '2',
-    title: 'South Apartment',
-    city: 'New York',
-    street: 'Time Squere',
-    category: 'apartment',
-    image: 'http://vim.placeholder.com/350x250',
-    bedrooms: 3,
-    description: 'Very nice apartment',
-    dailyRate: 34,
-    shared: false,
-    createdAt: '24/12/2012'
-  },
-  {
-    id: '3',
-    title: 'North Apartment',
-    city: 'New York',
-    street: 'Time Squere',
-    category: 'apartment',
-    image: 'http://vim.placeholder.com/350x250',
-    bedrooms: 3,
-    description: 'Very nice apartment',
-    dailyRate: 34,
-    shared: false,
-    createdAt: '24/12/2012'
-  },
-  {
-    id: '4',
-    title: 'West Apartment',
-    city: 'New York',
-    street: 'Time Squere',
-    category: 'apartment',
-    image: 'http://vim.placeholder.com/350x250',
-    bedrooms: 3,
-    description: 'Very nice apartment',
-    dailyRate: 34,
-    shared: false,
-    createdAt: '24/12/2012'
-  }];
-
-  public getRentalById(rentalId: string): Observable<Rental> {
-      return new Observable<Rental>((observer) => {
-        setTimeout(() => {
-            const foundRental = this.rentals.find((rental) => {
-                return rental.id === rentalId;
-            });
-
-            observer.next(foundRental);
-        }, 500);
-      });
+  constructor(private http: HttpClient) {
+    
   }
 
-  public getRentals(): Observable<Rental[]> {
-      return new Observable<Rental[]>((observer) => {
-        setTimeout(() => {
-            observer.next(this.rentals);
-        }, 1000);
-      });
-    }
+  public getRentalById(rentalId: string): Observable<any> {
+  return this.http.get('/api/v1/rentals/' + rentalId);
+  }
+
+  public getRentals(): Observable<any> {
+    return this.http.get('/api/v1/rentals');
+  }
 }
